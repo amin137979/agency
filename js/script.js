914166428c2a928ca309ff3e03a33fa871c708ef
coder-118 <72459712+amin137979@users.noreply.github.com>
2020-10-06 19:31:21 +0330

    agency site

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function changer () {
+	var y=document.getElementsByClassName("bibak");
+	y.style.display= "block";
+	
+	
+	
+	
+	/*  var toy =document.getElementById("date").innerHTML;
+	 document.getElementById("lang").innerHTML = toy; */
+	
+}
